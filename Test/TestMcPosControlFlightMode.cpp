66104a03f225b1acf7ae8caeef6b93f17a678ac5
RomanBapst <bapstroman@gmail.com>
2021-10-06 18:28:45 +0300

    added google test structure
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

---
+++ Test/TestMcPosControlFlightMode.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <proposal.hpp>
+
+
+
+TEST(McPosControlFlightMode, CanRun) {
+
+	VehicleState _vehicle_state;
+
+	StandardTiltrotor _tiltrotor_vehicle;
+
+	FlightModeManager _manager(_tiltrotor_vehicle);
+
+
+
+
+
+
+}
